691af636a061311278ca3a0147a678fe69c86e2f
DAMEON BRYANT <dameon@dameonbryant.com>
2017-03-09 07:55:15 -0800

    Initial Commit

---
+++ OpenWeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Dameon Bryant on 3/9/17.
+//  Copyright © 2017 Don't Quit On Yourself LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
